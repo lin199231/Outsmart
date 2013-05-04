4e6763ec9b4fe3346f05bb2e95a894416af1e182
凤翔 <fengxiang220@gmail.com>
2013-05-04 19:35:55 +0800

    增加标点过滤

--- src/findix/meetingreminder/analysis/GetUserLocation.java
+++ src/findix/meetingreminder/analysis/GetUserLocation.java
@@ -1,11 +1,21 @@
 package findix.meetingreminder.analysis;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import findix.meetingreminder.segmentation.NoPunctuation;
+import findix.meetingreminder.segmentation.NoStopword;
 import findix.meetingreminder.segmentation.SegmentationByBloom;
 
-
 public class GetUserLocation {
-	public String[] getLocation(String text){
-		SegmentationByBloom seg =new SegmentationByBloom();
-		return seg.getWords(text);	
+	public String[] getLocation(String text) {
+		SegmentationByBloom seg = new SegmentationByBloom();
+		ArrayList<String> location = seg.getWordsbyArrayList(text);
+		NoPunctuation np = new NoPunctuation();
+		NoStopword ns = new NoStopword();
+		np.getNoPunctuationWords(location);
+		ns.getNoStopwordWords(location);
+		Collections.reverse(location);
+		return (String[]) location.toArray(new String[location.size()]);
 	}
 }
